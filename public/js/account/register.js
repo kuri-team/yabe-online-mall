7c452cc8c4b3c104a820774965c215bd42113c1d
tnathu-ai <s3879312@rmit.edu.vn>
2021-04-22 09:47:06 +0700

    fixed bugs, the next button on contact form does not work

--- public/js/account/register.js
+++ public/js/account/register.js
@@ -10,15 +10,17 @@ const progressText = document.querySelectorAll(".step p");
 const progressCheck = document.querySelectorAll(".step .check");
 const bullet = document.querySelectorAll(".step .point");
 let current = 1;
+
 <!-- Created By CodingNepal -->
 nextBtnFirst.addEventListener("click", function(event){
     event.preventDefault();
-    slidePage.style.marginLeft = "-25%";
+    slidePage.style.marginLeft = "-50%";
     bullet[current - 1].classList.add("active");
     progressCheck[current - 1].classList.add("active");
     progressText[current - 1].classList.add("active");
     current += 1;
 });
+
 nextBtnSec.addEventListener("click", function(event){
     event.preventDefault();
     slidePage.style.marginLeft = "-50%";
@@ -27,6 +29,7 @@ nextBtnSec.addEventListener("click", function(event){
     progressText[current - 1].classList.add("active");
     current += 1;
 });
+
 nextBtnThird.addEventListener("click", function(event){
     event.preventDefault();
     slidePage.style.marginLeft = "-75%";
@@ -35,13 +38,14 @@ nextBtnThird.addEventListener("click", function(event){
     progressText[current - 1].classList.add("active");
     current += 1;
 });
+
 submitBtn.addEventListener("click", function(){
     bullet[current - 1].classList.add("active");
     progressCheck[current - 1].classList.add("active");
     progressText[current - 1].classList.add("active");
     current += 1;
     setTimeout(function(){
-        alert("Your Form Successfully Signed up");
+        alert("Successfully Registered");
         location.reload();
     },800);
 });
@@ -54,6 +58,7 @@ prevBtnSec.addEventListener("click", function(event){
     progressText[current - 2].classList.remove("active");
     current -= 1;
 });
+
 prevBtnThird.addEventListener("click", function(event){
     event.preventDefault();
     slidePage.style.marginLeft = "-25%";
@@ -62,6 +67,7 @@ prevBtnThird.addEventListener("click", function(event){
     progressText[current - 2].classList.remove("active");
     current -= 1;
 });
+
 prevBtnFourth.addEventListener("click", function(event){
     event.preventDefault();
     slidePage.style.marginLeft = "-50%";
